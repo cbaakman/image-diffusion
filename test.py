35ab89411308cfe37565edfac440d8ccea16fbd9
Coos Baakman <cbaakman@radboudumc.nl>
2024-03-25 14:33:59 +0100

    add a time channel to the model and increase the kernel size

--- test.py
+++ test.py
@@ -24,18 +24,23 @@ def square(x: float) -> float:
 
 
 class Model(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, T: int):
         super().__init__()
 
-        self.conv1 = torch.nn.Conv2d(3, 16, kernel_size=(10, 10), padding=5)
+        self.T = T
+
+        self.conv1 = torch.nn.Conv2d(4, 16, kernel_size=(32, 32), padding=16)
         self.act = torch.nn.ReLU()
-        self.conv2 = torch.nn.Conv2d(16, 3, kernel_size=(10, 10), padding=4)
+        self.conv2 = torch.nn.Conv2d(16, 3, kernel_size=(32, 32), padding=15)
 
     def forward(
         self,
         z: torch.Tensor,
         t: int,
     ):
+
+        t_channel = (t / self.T) * torch.ones(list(z.shape[:-1]) + [1], device=z.device, dtype=torch.float)
+        z = torch.cat((z, t_channel), dim=-1)
 
         z = z.transpose(-1, -2).transpose(-2, -3).unsqueeze(0)
 
@@ -61,28 +66,32 @@ if __name__ == "__main__":
 
     _log.debug(f"initializing model")
     T = 100
-    model = Model().to(device=device)
+    model = Model(T).to(device=device)
     if os.path.isfile("model.pth"):
         model.load_state_dict(torch.load("model.pth", map_location=device))
 
     _log.debug(f"initializing diffusion model optimizer")
     dm = DiffusionModelOptimizer(T, model)
 
-    nepoch = 1000
+    nepoch = 10000
+    lowest_loss = torch.tensor(1e99, device=device)
     for epoch_index in range(nepoch):
         _log.debug(f"starting epoch {epoch_index}")
 
-        dm.optimize(image)
+        loss = dm.optimize(image)
 
-        torch.save(model.state_dict(), "model.pth")
+        if loss < lowest_loss:
+            torch.save(model.state_dict(), "model.pth")
+            lowest_loss = loss
 
     model.load_state_dict(torch.load("model.pth", map_location=device))
 
     _log.debug(f"sampling")
-    alpha = dm.alpha_function(90)
+    alpha = dm.alpha_function(100)
     sigma = sqrt(1.0 - square(alpha))
     epsilon = torch.randn(image.shape, device=device)
     noised = alpha * image + sigma * epsilon
+    #noised = torch.randn(image.shape, device=device)
     imwrite("noised.png", (noised * 127.5 + 127.5).detach().numpy().astype("uint8"))
     image = torch.clamp(dm.sample(noised), 0.0, 1.0)
 
